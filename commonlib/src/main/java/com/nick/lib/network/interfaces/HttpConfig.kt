0b1f791bc3485f301c590b20aa365f033add01fd
nick <1085823261@qq.com>
2019-11-13 21:28:00 +0800

    修改代码

--- commonlib/src/main/java/com/nick/lib/network/interfaces/HttpConfig.kt
+++ commonlib/src/main/java/com/nick/lib/network/interfaces/HttpConfig.kt
@@ -2,7 +2,7 @@ package com.nick.lib.network.interfaces
 
 import okhttp3.OkHttpClient
 
-class HttpConfig {
-	fun baseUrl(): String = "https://www.baidu.com/"
-	fun okHttpConfig(okHttpBuilder: OkHttpClient.Builder): OkHttpClient = okHttpBuilder.build()
+open class HttpConfig {
+	open fun baseUrl(): String = "https://www.baidu.com/"
+	open fun okHttpConfig(okHttpBuilder: OkHttpClient.Builder): OkHttpClient = okHttpBuilder.build()
 }
