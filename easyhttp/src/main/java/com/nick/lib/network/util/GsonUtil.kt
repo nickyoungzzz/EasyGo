ca87771482f22133ec903bf9d8b3c46cdeb597a3
nick <1085823261@qq.com>
2019-11-20 23:30:24 +0800

    修改GsonUtil

--- easyhttp/src/main/java/com/nick/lib/network/util/GsonUtil.kt
+++ easyhttp/src/main/java/com/nick/lib/network/util/GsonUtil.kt
@@ -3,12 +3,20 @@
 package com.nick.lib.network.util
 
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
+import com.google.gson.JsonParser
 import org.json.JSONObject
 
 fun <T> String.parseAsObject(clazz: Class<T>): T = Gson().fromJson(this, clazz)
 
-fun <T> String.parseAsList(clazz: Class<T>): MutableList<T> = Gson().fromJson(this, object : TypeToken<List<T>>() {}.type)
+fun <T> String.parseAsList(clazz: Class<T>): MutableList<T> {
+	val list = arrayListOf<T>()
+	val jsonAny = JsonParser.parseString(this).asJsonArray
+	val gao = Gson()
+	jsonAny.forEach { obj ->
+		list.add(gao.fromJson(obj, clazz))
+	}
+	return list
+}
 
 fun Map<String, String>.toJsonString() = JSONObject(this).toString()
 
