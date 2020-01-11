573934aa3c5f1c1e13a46600f60c9f495ae9b3ac
nick <1085823261@qq.com>
2020-01-11 13:52:40 +0800

    去除多余代码

--- easyhttp/src/main/java/com/nick/easyhttp/util/GsonUtil.kt
+++ easyhttp/src/main/java/com/nick/easyhttp/util/GsonUtil.kt
@@ -3,21 +3,31 @@
 package com.nick.easyhttp.util
 
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.google.gson.JsonParser
 import org.json.JSONObject
 
-fun <T> String.parseAsObject(clazz: Class<T>): T = Gson().fromJson(this, clazz)
+fun <T> String.parseAsObject(clazz: Class<T>): T = GsonObject.create().fromJson(this, clazz)
 
 fun <T> String.parseAsList(clazz: Class<T>): MutableList<T> {
 	val list = arrayListOf<T>()
 	val jsonAny = JsonParser.parseString(this).asJsonArray
-	val gao = Gson()
 	jsonAny.forEach { obj ->
-		list.add(gao.fromJson(obj, clazz))
+		list.add(GsonObject.create().fromJson(obj, clazz))
 	}
 	return list
 }
 
 fun Map<String, String>.toJsonString() = JSONObject(this).toString()
 
-fun <T> objectToJsonString(t: T): String = Gson().toJson(t)
+fun <T> objectToJsonString(t: T): String = GsonObject.create().toJson(t)
+
+internal object GsonObject {
+	private lateinit var gao: Gson
+	fun create(): Gson {
+		if (!GsonObject::gao.isInitialized) {
+			gao = GsonBuilder().serializeNulls().create()
+		}
+		return gao
+	}
+}
